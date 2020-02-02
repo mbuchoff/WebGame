fe68623043a642f7c37572e8e0340c1e9ce48a70
Michael Buchoff <mbuchoff@gmail.com>
2020-02-02 10:52:47 -0500

    Forgot a file

---
+++ WebGame/Models/GameModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebGame.Models
+{
+    public class GameModel
+    {
+        public List<RoomModel> Rooms { get; } = new List<RoomModel>();
+    }
+}
